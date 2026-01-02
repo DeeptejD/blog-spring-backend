cbd8794111e9c02a3b05bb74f0a38a97f6886b8d
Deeptej <deeptejdhauskar2003@gmail.com>
2026-01-02 18:59:58 +0530

    add errorController and API error response DTO

---
+++ src/main/java/com/example/blog/controllers/ErrorController.java
@@ -0,0 +1,37 @@
+package com.example.blog.controllers;
+
+import com.example.blog.domain.dtos.ApiErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@ControllerAdvice
+@Slf4j
+public class ErrorController {
+    /**
+     * This intercepts exceptions thrown by any controllers before it reaches the client. When an exception is thrown Spring looks for an @ExceptionHandler. So that a proper ResponseEntity containing the ApiErrorResponse is returned to the client.
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleException(Exception ex) {
+        log.error("Caught exception", ex);
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected error occurred")
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage()) // this is fine because this isn't prod. it can expose certain system state.
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
