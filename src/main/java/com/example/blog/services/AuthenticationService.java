a1eda05009ed567af3b2faf14ddaaeccd3186900
Deeptej <deeptejdhauskar2003@gmail.com>
2026-01-04 19:52:48 +0530

    add authentication service interface and authentication controller

---
+++ src/main/java/com/example/blog/services/AuthenticationService.java
@@ -0,0 +1,8 @@
+package com.example.blog.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface AuthenticationService {
+    UserDetails authenticate(String email, String password); // recap, SS only understands UserDetails, hence in our authentication logic we are returning a UserDetails object.
+    String generateToken(UserDetails userDetails); // this will just generate our JWT.
+}
