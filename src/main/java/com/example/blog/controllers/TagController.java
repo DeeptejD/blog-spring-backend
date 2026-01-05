79cd2236b6681a8cfc104ac4e470573203b27777
Dhauskar, Deeptej (D.P.) <ddhauska@visteon.com>
2026-01-05 18:25:49 +0530

    add create and delete endpoints for tags

--- src/main/java/com/example/blog/controllers/TagController.java
+++ src/main/java/com/example/blog/controllers/TagController.java
@@ -1,16 +1,19 @@
 package com.example.blog.controllers;
 
+import com.example.blog.domain.dtos.CreateTagsRequest;
 import com.example.blog.domain.dtos.TagResponse;
 import com.example.blog.domain.entities.Tag;
 import com.example.blog.mappers.TagMapper;
 import com.example.blog.services.TagService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/tags")
@@ -24,5 +27,25 @@ public class TagController {
         List<Tag> tags = tagService.getTags();
         List<TagResponse> tagResponses = tags.stream().map(tagMapper::toTagResponse).toList();
         return ResponseEntity.ok(tagResponses);
+    }
+
+    @PostMapping
+    public ResponseEntity<List<TagResponse>> createTags(@RequestBody @Valid CreateTagsRequest createTagsRequest) {
+        List<TagResponse> createdTags = tagService.createTags(createTagsRequest.getNames())
+                .stream().map(tagMapper::toTagResponse)
+                .toList();
+
+        System.out.println(createdTags);
+
+        return new ResponseEntity<>(
+                createdTags,
+                HttpStatus.CREATED
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTags(@PathVariable UUID id) {
+        tagService.deleteTag(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/example/blog/services/impl/TagServiceImpl.java
+++ src/main/java/com/example/blog/services/impl/TagServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.blog.services.impl;
 import com.example.blog.domain.entities.Tag;
 import com.example.blog.repositories.TagRepository;
 import com.example.blog.services.TagService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +18,43 @@ public class TagServiceImpl implements TagService {
     @Override
     public List<Tag> getTags() {
         return tagRepository.findAllWithPostCount();
+    }
+
+    @Transactional
+    @Override
+    public List<Tag> createTags(Set<String> tagNames) {
+        List<Tag> existingTags = tagRepository.findByNameIn(tagNames);
+
+        Set<String> existingTagNames = existingTags.stream().map(Tag::getName).collect(Collectors.toSet());
+        // filter out only new tag names to create
+
+        List<Tag> newTags = tagNames.stream()
+                .filter(name -> !existingTagNames.contains(name))
+                .map(name -> Tag.builder()
+                        .name(name)
+                        .posts(new HashSet<>())
+                        .build())
+                .toList();
+
+
+        List<Tag> savedTags = new ArrayList<>();
+        if (!newTags.isEmpty()) {
+            savedTags = tagRepository.saveAll(newTags);
+        }
+
+        savedTags.addAll(existingTags); // add new to existing tags
+
+        return savedTags;
+    }
+
+    @Transactional
+    @Override
+    public void deleteTag(UUID id) {
+        tagRepository.findById(id).ifPresent( tag -> {
+            if(!tag.getPosts().isEmpty()) {
+                throw new IllegalStateException("Cannot delete tag with posts");
+            }
+            tagRepository.deleteById(id);
+        });
     }
 }
